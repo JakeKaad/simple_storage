ab73ba8c826e7fede535cf0bad4c004bda47441f
jkaad <jkaad@focusvision.com>
2019-05-11 08:10:47 -0700

    Set up project, adds engine

---
+++ README.md
@@ -0,0 +1,4 @@
+install and use ruby 2.6.3 using RVM or something similar
+
+change permission on the store file
+chmod +x scripts/store
---
+++ lib/simple_store/engine.rb
@@ -0,0 +1,59 @@
+module SimpleStore
+  class Engine
+    COMMANDS = [
+      'SET',
+      'GET',
+      'DELETE',
+      'COUNT',
+      'BEGIN',
+      'COMMIT',
+      'ROLLBACK',
+      'EXIT'
+    ]
+
+    attr_reader :command
+
+    def run
+      system("clear")
+      puts 'Welcome to Simple Store'
+      puts 'SET <key> <value> // store the value for key'
+      puts 'GET <key> // return the current value for key'
+      puts 'DELETE <key> // remove the entry for key'
+      puts 'COUNT <value> // return the number of keys that have the given value'
+      puts 'BEGIN // start a new transaction'
+      puts 'COMMIT // complete the current transaction'
+      puts 'ROLLBACK // revert to state prior to BEGIN call'
+      puts 'EXIT // exits Simple Store'
+
+      begin do
+        puts "What would you like to do?"
+        get_current_command
+
+        unless valid_command?
+          puts 'please select a valid command'
+          next
+        end
+
+        exit if exiting?
+      end
+    end
+
+    private
+
+    def valid_command?
+      COMMANDS.include?(command)
+    end
+
+    def get_current_command
+      @command = gets.chomp
+    end
+
+    def exit
+
+    end
+
+    def exiting?
+      command == 'EXIT'
+    end
+  end
+end
